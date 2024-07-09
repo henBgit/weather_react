4d0a161dba64fb736fb600a930b55d3f0146d5a5
henBgit <henborochov2@gmail.com>
2024-07-09 20:09:03 +0300

    first commite

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="app-container">
       <CssBaseline />
-      <Router>
+   
         <Header />
     
           <Routes>
@@ -19,7 +19,7 @@ function App() {
             
           </Routes>
        
-      </Router>
+    
     </div>
   );
 }
